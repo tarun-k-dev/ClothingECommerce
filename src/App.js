8711ac1b16b419002c9856cafa50d57e80516713
Tarun Kumar <51148612+tarun-k-dev@users.noreply.github.com>
2023-02-19 18:39:27 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,52 @@
+const App = () => {
+  return (
+    <>
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Womens</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Mens</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
